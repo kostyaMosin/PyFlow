5b9619260c83c4e7018a370757f81b5b86f066ad
Kostya Mosin <mkv150293@gmail.com>
2021-04-15 23:09:09 +0300

    add user profile view and template

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import SignUpView
+from .views import SignUpView, view_user_profile
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
+    path('profile/', view_user_profile, name='profile'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,39 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.db.models import Count, F, Sum
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import generic
+
+from pyflow.models import Post, Comment, Tag
 
 
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
+
+
+def view_user_profile(request):
+    if request.user.is_authenticated:
+        user = request.user
+        posts = Post.objects.filter(user=user)
+        comments = Comment.objects.filter(user=user)
+        tags = Tag.objects.filter(posts__user=user).annotate(posts_count=Count(F('posts'))).order_by('-posts_count')
+        posts_likes = posts.aggregate(likes=Sum(F('likes__value')))['likes']
+        posts_likes = posts_likes if posts_likes else 0
+        posts_shows = posts.aggregate(count=Count(F('shows')))['count']
+        posts_shows = posts_shows if posts_shows else 0
+        comments_likes = comments.aggregate(likes=Sum(F('likes__value')))['likes']
+        comments_likes = comments_likes if comments_likes else 0
+        context = {
+            'user': user,
+            'posts': posts.order_by('-create_at'),
+            'comments': comments,
+            'tags': tags,
+            'reputation': sum([posts_likes, posts_shows, comments_likes]),
+            'posts_shows': posts_shows,
+            'posts_likes': posts_likes,
+            'comments_likes': comments_likes,
+        }
+        return render(request, 'profile.html', context)
+    return redirect('login')

00bf0e972e7ed2c1c1f0b2bc22857b673f8841d5
kostyaMosin <mkv150293@gmail.com>
2021-04-16 10:24:08 +0300

    change max length tag title 30 -> 255

--- pyflow/models.py
+++ pyflow/models.py
@@ -99,7 +99,7 @@ class CommentLike(models.Model):
 
 
 class Tag(models.Model):
-    title = models.CharField(max_length=30)
+    title = models.CharField(max_length=255)
     create_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
